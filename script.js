a5b4ce345282141f7efa718e051cac82699ecdda
Kuzemica <153897030+Kuzemica@users.noreply.github.com>
2025-07-26 00:21:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const toggleBtn = document.getElementById("toggle-btn");
+const toggle = document.getElementById("darkToggle");
 
-const currentMode = localStorage.getItem("mode");
-if (currentMode === "dark") {
-  document.body.classList.add("dark");
-  toggleBtn.textContent = "🌞 Chế độ sáng";
-}
-
-toggleBtn.addEventListener("click", () => {
+toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-
   const isDark = document.body.classList.contains("dark");
-  toggleBtn.textContent = isDark ? "🌞 Chế độ sáng" : "🌓 Chế độ tối";
   localStorage.setItem("mode", isDark ? "dark" : "light");
 });
+
+if (localStorage.getItem("mode") === "dark") {
+  document.body.classList.add("dark");
+}
