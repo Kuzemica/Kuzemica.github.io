9ac9028aaf5002a234bd5982e17be1a79e63f4c6
Kuzemica <153897030+Kuzemica@users.noreply.github.com>
2025-07-25 23:45:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const toggleBtn = document.getElementById("toggle-btn");
 
-// Kiểm tra chế độ đã lưu
 const currentMode = localStorage.getItem("mode");
 if (currentMode === "dark") {
   document.body.classList.add("dark");
   toggleBtn.textContent = "🌞 Chế độ sáng";
 }
 
-// Bắt sự kiện khi click
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
   const isDark = document.body.classList.contains("dark");
   toggleBtn.textContent = isDark ? "🌞 Chế độ sáng" : "🌓 Chế độ tối";
-
-  // Lưu lại chế độ
   localStorage.setItem("mode", isDark ? "dark" : "light");
 });
